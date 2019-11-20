a2e56b55551ac27540099a0adb3413256b82bcc4
AshrithaG <57980659+AshrithaG@users.noreply.github.com>
2019-11-20 16:02:57 +0530

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a=5, b=3, c;
+   
+   
+   cin >> a >> b;
+
+   c = a + b;
+   cout <<"Sum of the numbers: " << c << endl;
+   
+   return 0;
+}
