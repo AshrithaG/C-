81bb065a5ec7ef658eae9c1cb1283665eb6f8747
AshrithaG <57980659+AshrithaG@users.noreply.github.com>
2019-11-20 15:15:28 +0530

    Add files via upload

---
+++ a3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double firstNumber, secondNumber, productOfTwoNumbers;
+   
+   
+    cin >> 5>> 6;
+ 
+    
+    productOfTwoNumbers = 5*6;  
+    cout << "Product = " << productOfTwoNumbers;    
+    
+    return 0;
+}
